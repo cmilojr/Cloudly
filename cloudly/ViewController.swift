0c7dfe5c476e8bd55b9d12068c99461ce0064a99
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2020-08-14 12:11:16 -0500

    Initial Commit

---
+++ cloudly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cloudly
+//
+//  Created by Camilo Jiménez on 14/08/20.
+//  Copyright © 2020 Camilo Jiménez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
